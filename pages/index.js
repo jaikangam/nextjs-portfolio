2740d2383a651928ddcb5078517ded15bb1a54af
jaikangam <jaikangam12@gmail.com>
2022-06-17 23:05:10 +0530

    Added Skill and About

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import About from '../components/About'
+import Skills from '../components/Skills'
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
 
       <Navbar /> 
       <Main />
+      <About />
+      <Skills />
 
     </div>
   )
